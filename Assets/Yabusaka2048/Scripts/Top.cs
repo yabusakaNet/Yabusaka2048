cd8e1a7b17d400816ba52742bd7c910224201d83
Yamanaka Sho <ls279702@gmail.com>
2020-10-08 12:12:27 +0900

    レイアウト変更

---
+++ Assets/Yabusaka2048/Scripts/Top.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Top : MonoBehaviour
+{
+    public void OnRestart()
+    {
+        UserProgress.Current.GetGameState<GameState>(UserProgress.Current.CurrentGameId).SetField(new int[0]);
+        UserProgress.Current.SaveGameState(UserProgress.Current.CurrentGameId);
+        UserProgress.Current.Save();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene("Game");
+    }
+    public void OnRetry()
+    {   
+        SceneManager.LoadScene("Game");
+    }
+}
