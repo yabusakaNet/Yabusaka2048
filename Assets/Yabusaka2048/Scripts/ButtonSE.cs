264180aafcf63712a9386edc26dede76955a57d2
Yamanaka Sho <ls279702@gmail.com>
2020-10-19 12:15:06 +0900

    ボタンSE追加

---
+++ Assets/Yabusaka2048/Scripts/ButtonSE.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSE : MonoBehaviour
+{
+    private AudioSource buttonSE;
+
+    void Start()
+    {
+        buttonSE = GetComponent<AudioSource>();
+    }
+
+    public void OnClick()
+    {
+        buttonSE.PlayOneShot(buttonSE.clip);
+    }
+}
