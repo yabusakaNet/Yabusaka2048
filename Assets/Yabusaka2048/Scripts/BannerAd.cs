101726dc90b7a0ec3bd96c7102dba898135a95d4
Yamanaka Sho <ls279702@gmail.com>
2020-10-28 12:59:08 +0900

    リリース用素材追加

---
+++ Assets/Yabusaka2048/Scripts/BannerAd.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class BannerAd : MonoBehaviour
+{
+    private BannerView bannerView;
+
+    public void Start()
+    {
+        #if UNITY_ANDROID
+            string appId = "ca-app-pub-6239259349327423~6366977548";
+        #elif UNITY_IPHONE
+            string appId = "ca-app-pub-3940256099942544~1458002511";
+        #else
+            string appId = "unexpected_platform";
+        #endif
+
+        // Initialize the Google Mobile Ads SDK.
+        MobileAds.Initialize(appId);
+
+        this.RequestBanner();
+    }
+
+    private void RequestBanner()
+    {
+        #if UNITY_ANDROID
+            string adUnitId = "ca-app-pub-6239259349327423/2946607284";
+        #elif UNITY_IPHONE
+            string adUnitId = "ca-app-pub-3940256099942544/2934735716";
+        #else
+            string adUnitId = "unexpected_platform";
+        #endif
+
+        // Create a 320x50 banner at the top of the screen.
+        this.bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Bottom);
+        // AdSize adSize = new AdSize(250, 250);
+        // BannerView bannerView = new BannerView(adUnitId, adSize, AdPosition.Bottom);
+
+         // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+
+        // Load the banner with the request.
+        this.bannerView.LoadAd(request);
+    }
+}
